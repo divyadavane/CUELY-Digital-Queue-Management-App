adecd7f7c67e01ad931301c479b4991353d2b4e7
divdavane@gmail.com <divya.235937202@vcet.edu.in>
2026-08-07 16:31:27 +0530

    Fix QR code linking error by forcing stable webVersionCache

--- server/whatsapp-service.js
+++ server/whatsapp-service.js
@@ -39,6 +39,10 @@ const client = new Client({
   authStrategy: new LocalAuth({
     dataPath: './.wwebjs_auth'
   }),
+  webVersionCache: {
+    type: 'remote',
+    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+  },
   puppeteer: {
     headless: true,
     executablePath: browserPath,
